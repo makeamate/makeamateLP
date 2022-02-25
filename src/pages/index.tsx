b2d7627421ffd496c69630cd661fd3cb42be8b53
Alesancab1 <alesancab@alum.us.es>
2022-02-25 18:36:12 +0100

    ACTUALIZACION

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import AuthorSection from "../components/sections/AuthorSection";
 import AboutSection from "../components/sections/AboutSection";
 import FooterSection from "../components/sections/FooterSection";
 import ProjectsSection from "../components/sections/ProjectsSection";
+import TimerSection from "../components/sections/TimerSection";
 
 const IndexPage: React.FC = () => {
   return (
@@ -15,6 +16,7 @@ const IndexPage: React.FC = () => {
       <AuthorSection />
       <AboutSection />
       <ProjectsSection />
+      <TimerSection />
       <FooterSection />
     </Layout>
   );
