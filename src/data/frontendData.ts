6db19c74050eaf776dd18c346c92e0282564fade
raupargor <raupargor@alum.us.es>
2022-02-28 23:11:14 +0100

    UPDATE TEAM

--- src/data/frontendData.ts
+++ src/data/frontendData.ts
@@ -4,56 +4,56 @@ export const projects = [
         alt: "Alejandro Sanabria preview",
         title: "Alejandro Sanabria",
         desc:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        github: "https://github.com/jesrolcad/",
+          "Se trata del coordinador del equipo de frontend. Destacan su habilidades de comunicación y liderazgo, con lo que consigue sacar lo mejor de cada miembro del equipo.",
+        github: "https://github.com/alesancab1/",
     },
     {
         img: "images/works/belidia.png",
         alt: "María García preview",
         title: "María García",
         desc:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        github: "https://github.com/jesrolcad/",
+          "María cuenta con una gran vision espacial. Es una persona perfeccionista e implicada, con la que contamos en el equipo para desarrollar una interfaz limpia y pulida",
+        github: "https://github.com/margarcac1/",
     },
     {
-        img: "images/works/belidia.png",
+        img: "images/works/Raul.png",
         alt: "Raúl Parrado preview",
         title: "Raúl Parrado",
         desc:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        github: "https://github.com/jesrolcad/",
+          "Ejemplo de esfuerzo y dedicación. Su creatividad y compañerismo aportan gran valor al equipo de frontend en el desarrollo de sus actividades",
+        github: "https://github.com/raupargor/",
     },
     {
       img: "images/works/belidia.png",
       alt: "Francisco Rodriguez preview",
       title: "Francisco Rodriguez",
       desc:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      github: "https://github.com/jesrolcad/",
+        "Project Manager del equipo. Demuestra su constancia y capacidades técnicas, además de aportar un apoyo a los compañeros en las dificultades técnicas.",
+      github: "https://github.com/FranciscoJRE14/",
     },
     {
         img: "images/works/belidia.png",
         alt: "Iván Romero preview",
         title: "Iván Romero",
         desc:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        github: "https://github.com/jesrolcad/",
+          "Se trata de una persona con una gran capacidad analítica, muy versátil y dinámica, siempre hace aportaciones de valor que llevan al equipo a llegar más alla de sus metas.",
+        github: "https://github.com/ivaromcos/",
     },
     {
         img: "images/works/belidia.png",
         alt: "Adriana León preview",
         title: "Adriana León",
         desc:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        github: "https://github.com/jesrolcad/",
+          "Adriana es una excelente fuente de creatividad, tiene experiencia con el diseño gráfico y es una excelente persona para ocupar un lugar en el este equipo gracias a sus habilidades.",
+        github: "https://github.com/adrleogom/",
     },
     {
         img: "images/works/belidia.png",
         alt: "Siamion Danko preview",
         title: "Siamion Danko",
         desc:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        github: "https://github.com/jesrolcad/",
+          "Siamion es un recurso fundamental en el equipo, tiene conocimientos técnicos avanzados sobre las tecnologías utilizadas y ayuda tomar buenas decisiones, siempre fundamentadas gracias a su experiencia.",
+        github: "https://github.com/siadan/",
     }
   ];
   
